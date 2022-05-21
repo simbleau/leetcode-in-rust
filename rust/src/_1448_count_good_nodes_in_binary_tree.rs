6d8578e0f7c2045de0dfc8b2cd3598802aa237af
Spencer Imbleau <spencer@imbleau.com>
2022-05-21 16:05:41 -0400

    Added 1448

---
+++ rust/src/_1448_count_good_nodes_in_binary_tree.rs
@@ -0,0 +1,159 @@
+use std::{cell::RefCell, rc::Rc};
+
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+pub fn count_good_nodes(node: &Rc<RefCell<TreeNode>>, max: i32) -> i32 {
+    let mut good_nodes = 0;
+
+    // Am I a good node?
+    if node.borrow().val >= max {
+        good_nodes += 1;
+    }
+
+    // Are my children good nodes?
+    let new_max = std::cmp::max(max, node.borrow().val);
+    match node.borrow().left.as_ref() {
+        Some(left) => {
+            good_nodes += count_good_nodes(left, new_max);
+        }
+        None => (),
+    };
+    match node.borrow().right.as_ref() {
+        Some(right) => {
+            good_nodes += count_good_nodes(right, new_max);
+        }
+        None => (),
+    };
+
+    good_nodes
+}
+
+pub fn good_nodes(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+    match root {
+        Some(root) => return count_good_nodes(&root, root.borrow().val),
+        None => return 0,
+    }
+}
+#[cfg(test)]
+#[test]
+fn test_1() {
+    //     [3]
+    //     / \
+    //    1  [4]
+    //   /   / \
+    // [3]  1  [5]
+    let root = Rc::new(RefCell::new(TreeNode {
+        val: 3,
+        left: Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 3,
+                left: None,
+                right: None,
+            }))),
+            right: None,
+        }))),
+        right: Some(Rc::new(RefCell::new(TreeNode {
+            val: 4,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: None,
+                right: None,
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 5,
+                left: None,
+                right: None,
+            }))),
+        }))),
+    }));
+    assert_eq!(good_nodes(Some(root)), 4);
+}
+
+#[test]
+fn test_2() {
+    //     [3]
+    //     /
+    //   [3]
+    //   / \
+    // [4]  2
+    let root = Rc::new(RefCell::new(TreeNode {
+        val: 3,
+        left: Some(Rc::new(RefCell::new(TreeNode {
+            val: 3,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 4,
+                left: None,
+                right: None,
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 2,
+                left: None,
+                right: None,
+            }))),
+        }))),
+        right: None,
+    }));
+    assert_eq!(good_nodes(Some(root)), 3);
+}
+
+#[test]
+fn test_3() {
+    // [1]
+    let root = Rc::new(RefCell::new(TreeNode {
+        val: 1,
+        left: None,
+        right: None,
+    }));
+    assert_eq!(good_nodes(Some(root)), 1);
+}
+
+#[test]
+fn test_4() {
+    //  [2]
+    //    \
+    //    [4]
+    //    / \
+    //  [8] [10]
+    //  /
+    // 4
+    let root = Rc::new(RefCell::new(TreeNode {
+        val: 2,
+        left: None,
+        right: Some(Rc::new(RefCell::new(TreeNode {
+            val: 4,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 8,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 4,
+                    left: None,
+                    right: None,
+                }))),
+                right: None,
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 10,
+                left: None,
+                right: None,
+            }))),
+        }))),
+    }));
+    assert_eq!(good_nodes(Some(root)), 4);
+}
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -5,16 +5,17 @@
 
 pub mod _121_best_time_to_buy_and_sell_stock;
 pub mod _1304_find_n_unique_integers_sum_up_to_zero;
+pub mod _1448_count_good_nodes_in_binary_tree;
 pub mod _1647_minimum_deletions_to_make_character_frequencies_unique;
 pub mod _1_two_sum;
 pub mod _20_valid_parentheses;
 pub mod _217_contains_duplicate;
 pub mod _238_product_of_array_except_self;
 pub mod _242_valid_anagram;
+pub mod _253_meeting_rooms_ii;
 pub mod _322_coin_change;
 pub mod _3_longest_substring_without_repeating_characters;
 pub mod _53_maximum_subarray;
-pub mod _253_meeting_rooms_ii;
 
 // Template for a solution
 pub fn template() {}
