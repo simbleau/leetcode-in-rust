f952ed1fdb6c9a13eb0c92958762fe65bc07df35
Spencer Imbleau <spencer@imbleau.com>
2022-05-25 06:48:07 -0400

    Fixed import

--- rust/src/_1448_count_good_nodes_in_binary_tree.rs
+++ rust/src/_1448_count_good_nodes_in_binary_tree.rs
@@ -1,23 +1,6 @@
 use std::{cell::RefCell, rc::Rc};
 
-// Definition for a binary tree node.
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-    pub val: i32,
-    pub left: Option<Rc<RefCell<TreeNode>>>,
-    pub right: Option<Rc<RefCell<TreeNode>>>,
-}
-
-impl TreeNode {
-    #[inline]
-    pub fn new(val: i32) -> Self {
-        TreeNode {
-            val,
-            left: None,
-            right: None,
-        }
-    }
-}
+use crate::TreeNode;
 
 pub fn count_good_nodes(node: &Rc<RefCell<TreeNode>>, max: i32) -> i32 {
     let mut good_nodes = 0;
