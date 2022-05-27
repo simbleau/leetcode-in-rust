8dcc5d8517f09c1ebf822e36357cc6610e2eb6a0
Spencer Imbleau <spencer@imbleau.com>
2022-05-27 12:50:25 -0400

    Added 39

---
+++ rust/src/_0039_combination_sum.rs
@@ -0,0 +1,37 @@
+use std::{collections::HashMap, vec};
+
+pub fn backtrack(
+    candidates: &Vec<i32>,
+    start: usize,
+    combination: &mut Vec<i32>,
+    remaining: i32,
+    results: &mut Vec<Vec<i32>>,
+) {
+    if remaining == 0 {
+        results.push(combination.clone());
+        return;
+    } else if remaining < 0 {
+        return;
+    }
+
+    for i in start..candidates.len() {
+        let num = candidates[i];
+        combination.push(num);
+        backtrack(candidates, i, combination, remaining - num, results);
+        combination.pop();
+    }
+}
+
+pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+    let mut results = vec![];
+    backtrack(&candidates, 0, &mut vec![], target, &mut results);
+    results
+}
+#[cfg(test)]
+#[test]
+fn test_1() {
+    assert_eq!(
+        combination_sum(vec![2, 3, 6, 7], 7),
+        vec![vec![2, 2, 3], vec![7]]
+    );
+}
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -28,6 +28,7 @@ pub mod _1239_maximum_length_of_a_concatenated_string_with_unique_characters;
 pub mod _1304_find_n_unique_integers_sum_up_to_zero;
 pub mod _1448_count_good_nodes_in_binary_tree;
 pub mod _1647_minimum_deletions_to_make_character_frequencies_unique;
+pub mod _0039_combination_sum;
 
 // Template for a solution
 pub fn template() {}
